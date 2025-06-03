94bdac2978614039d6c29f974b94f3bed130956f
Ciprian Cozma <cozma_ciprian19@yahoo.com>
2025-06-03 11:42:51 +0300

    added first exercise search element

---
+++ interview_practice/search_element.js
@@ -0,0 +1,14 @@
+const arr = [4, 2, 0, 10, 8, 30];
+
+const searchElement = (arr, elem) => {
+    for(let i = 0; i <= arr.length; i++) {
+        if(arr[i] == elem) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(searchElement(arr, 4));
+console.log(searchElement(arr, 10));
+console.log(searchElement(arr, 49));
