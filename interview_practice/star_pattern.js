0d2e594930c44b939340abbeb425c6392f4d79cb
Ciprian Cozma <cozma_ciprian19@yahoo.com>
2025-06-11 01:23:21 +0300

    updated star pattern file

--- interview_practice/star_pattern.js
+++ interview_practice/star_pattern.js
@@ -1,5 +1,4 @@
 // Star pattern 1
-
 // let n = 4;
 // for(let i = 0; i < n; i++) {
 //     let row = " ";
@@ -10,19 +9,28 @@
 // }
 
 // Star pattern 2
+// let n = 4;
 
-let n = 4;
+// for(let i = 0; i < n; i++) {
+//     let row = "";
 
-for(let i = 0; i < n; i++) {
-    let row = "";
+//     for(let j = 0; j < i + 1 ; j++) {
+//         row += " * ";
+//     }
 
-    for(let j = 0; j < i + 1 ; j++) {
-        row += " * ";
-    }
-
-    console.log(row);
-}
+//     console.log(row);
+// }
 
 // Star pattern 3
+// let n = 5;
 
+// for(let i = 0; i < n; i++) {
+//     let row = " ";
 
+//     for(let j = 0; j < i + 1; j++) {
+//         row = row + " " + (j + 1);
+//     }
+//     console.log(row);
+// }
+
+// Star pattern 4
